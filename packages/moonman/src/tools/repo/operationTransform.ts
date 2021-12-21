609e399e08aca1a14461098f77d718e1aaf2fa98
湫曗 <autumnsilverleaves@gmail.com>
2021-12-22 01:04:32 +0800

    feat: rankedViewList

--- packages/moonman/src/tools/repo/operationTransform.ts
+++ packages/moonman/src/tools/repo/operationTransform.ts
@@ -1,4 +1,10 @@
-import { IPieceView, IPropsMark, TOperation } from '../../operation'
+import {
+  IIdentity,
+  IInsertMark,
+  IPieceView,
+  IPropsMark,
+  TOperation,
+} from '../../operation'
 import { PieceView } from './pieceView'
 import { DataRepo } from './dataRepo'
 
@@ -43,6 +49,55 @@ export class OperationTransform {
       }
       return acc
     }, [] as IPropsMark[])
+  }
+
+  get insertMark() {
+    return this.operationList.reduce((acc, op) => {
+      if (op.type === 'IInsertMark') {
+        acc.push(op)
+      }
+      return acc
+    }, [] as IInsertMark[])
+  }
+
+  get rankedViewList() {
+    const pieceViewList = this.pieceViewList
+
+    const rankedView: PieceView<any>[] = []
+    this.insertMark.forEach((insert, index) => {
+      const view = pieceViewList.find((v) => {
+        return v.identity === insert.pieceView
+      })!
+
+      // 插入父元素
+      if (insert.relationAdress.relation.isInner) {
+        if (insert.relationAdress.relation.isForward) {
+          rankedView.unshift(view!)
+        } else {
+          rankedView.push(view!)
+        }
+      } else {
+        // 连接
+        const preViewIndex = rankedView.findIndex((v) => {
+          return v.identity === insert.relationAdress.adress.coordinate[0]
+        })
+
+        const preView = rankedView[preViewIndex]
+
+        const [before, after] = preView.splitByRelationAdress(
+          insert.relationAdress,
+        )
+
+        const newSlice = [before, view, after]
+
+        newSlice.filter((item) => {
+          return Boolean(item.length)
+        })
+
+        rankedView.splice(preViewIndex, 1, ...newSlice)
+      }
+    })
+    return rankedView
   }
 
   addOperation(opList: TOperation[], dataRepo: DataRepo = this.dataRepo) {
