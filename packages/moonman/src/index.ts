7213c326876a03b800b47a9b67856c53a22adcb3
湫曗 <autumnsilverleaves@gmail.com>
2022-01-02 16:10:33 +0800

    feat:  desc

--- packages/moonman/src/index.ts
+++ packages/moonman/src/index.ts
@@ -1,1 +1,108 @@
 export {}
+/**
+ * 插入元素
+ * 兄弟元素插在 ReplacedElement 上
+ * 子元素插在 Element 上
+ */
+interface IInsertChildForElementOperation {
+  type: 'insert-child'
+  nextId: string
+  position: 'front-flow' | 'back-flow'
+}
+interface IInsertBrotherForReplacedElementOperation {
+  type: 'insert-brother'
+  nextId: string
+  position: 'front-flow' | 'back-flow'
+  // 文字节点会用到
+  offsetIndex?: number
+}
+
+/**
+ * 指针式 Mark
+ * for element
+ * for replaced element
+ */
+interface IMarkPointerForElementOperation {
+  type: 'start-mark-pointer' | 'end-mark-pointer'
+  markId: string
+  // for text
+  offsetIndex: number
+}
+interface IMarkPointerForReplacedElementOperation {
+  type: 'start-replaced-mark-pointer' | 'end-replaced-mark-pointer'
+  markId: string
+}
+
+/**
+ * 元素 mark
+ * for element
+ * for replaced element
+ */
+interface IElementMarkOperation {
+  type: 'element-mark'
+  markId: string
+}
+
+interface IReplacedElementMarkOperation {
+  type: 'replaced-element-mark'
+  markId: string
+}
+
+/**
+ * for table
+ * 只给 table element
+ */
+interface ITeleportInsertChildForTableElementOperation {
+  type: 'teleport-insert-child'
+  nextId: string
+  position: 'front-flow' | 'back-flow'
+  coordinate: string[]
+}
+
+interface ITeleportMarkPointerForTableElementOperation {
+  type: 'start-teleport-mark-pointer' | 'end-teleport-mark-pointer'
+  markId: string
+  coordinate: string[]
+}
+
+/**
+ * 真实会改变节点内容的操作
+ */
+type TElementOperation =
+  | IInsertChildForElementOperation
+  | IMarkPointerForElementOperation
+  | ITeleportInsertChildForTableElementOperation
+  | IElementMarkOperation
+  | ITeleportMarkPointerForTableElementOperation
+
+type TReplacedElementOperation =
+  | IInsertBrotherForReplacedElementOperation
+  | IReplacedElementMarkOperation
+  | IMarkPointerForReplacedElementOperation
+
+export interface IReplacedElement {
+  id: string
+  src: string
+  type: string
+  operation?: TReplacedElementOperation[]
+}
+
+// for text
+export interface IPieceReplacedElement extends IReplacedElement {
+  range?: {
+    start?: number
+    end?: number
+  }
+}
+
+export interface IElement {
+  id: string
+  type: string
+  operation?: TElementOperation[]
+  content?: string
+}
+
+export interface IMarkData {
+  id: string
+  data: Record<string, any>
+}
---
+++ packages/moonman/src/temp.ts
@@ -0,0 +1,392 @@
+import { IElement, IMarkData, IPieceReplacedElement, IReplacedElement } from '.'
+
+const rDoc: IReplacedElement = {
+  id: 'r-doc0',
+  type: 'doc',
+  src: 'e-doc0',
+}
+
+const eDoc: IElement = {
+  id: 'e-doc0',
+  type: 'doc',
+  operation: [
+    {
+      type: 'insert-child',
+      nextId: 'r-par0',
+      position: 'back-flow',
+    },
+  ],
+}
+
+const rPar: IReplacedElement = {
+  id: 'r-par0',
+  type: 'paragraph',
+  src: 'e-par0',
+  operation: [
+    {
+      type: 'insert-brother',
+      nextId: 'r-table0',
+      position: 'back-flow',
+    },
+  ],
+}
+
+const ePar: IElement = {
+  id: 'e-par0',
+  type: 'paragraph',
+  operation: [
+    {
+      type: 'insert-child',
+      nextId: 'r-mention-zanlei-0',
+      position: 'back-flow',
+    },
+  ],
+}
+
+const rMentionZanlei: IReplacedElement = {
+  id: 'r-mention-zanlei-0',
+  type: 'mention',
+  src: 'e-mention-zanlei-0',
+  operation: [
+    {
+      type: 'insert-brother',
+      nextId: 'r-文字0',
+      position: 'back-flow',
+    },
+  ],
+}
+
+const eMentionZanlei: IElement = {
+  id: 'e-mention-zanlei-0',
+  type: 'mention',
+  operation: [],
+}
+
+const r文字: IPieceReplacedElement = {
+  id: 'r-文字0',
+  type: 'text',
+  src: 'e-文字0',
+  operation: [
+    {
+      type: 'insert-brother',
+      nextId: 'r-image-zhumulangma-0',
+      position: 'back-flow',
+    },
+  ],
+}
+
+const e文字: IElement = {
+  id: 'e-文字0',
+  type: 'text',
+  content: '我们去',
+}
+
+const rImageZhumulangma: IReplacedElement = {
+  id: 'r-image-zhumulangma-0',
+  type: 'image',
+  src: 'e-image-zhumulangma-0',
+  operation: [
+    {
+      type: 'insert-brother',
+      nextId: 'r-文字1',
+      position: 'back-flow',
+    },
+  ],
+}
+
+const eImageZhumulangma: IElement = {
+  id: 'e-image-zhumulangma-0',
+  type: 'image',
+  operation: [],
+}
+
+const r文字1: IPieceReplacedElement = {
+  id: 'r-文字1',
+  type: 'text',
+  src: 'e-文字1',
+  operation: [
+    {
+      type: 'insert-brother',
+      nextId: 'r-flow-chart-0',
+      position: 'back-flow',
+    },
+    {
+      type: 'insert-brother',
+      nextId: 'r-文字2',
+      position: 'back-flow',
+      offsetIndex: 1,
+    },
+  ],
+}
+
+const e文字1: IElement = {
+  id: 'e-文字1',
+  type: 'text',
+  content: '这是安排',
+  operation: [
+    {
+      type: 'end-mark-pointer',
+      markId: 'mark-0',
+      offsetIndex: 3,
+    },
+  ],
+}
+
+const r文字2: IPieceReplacedElement = {
+  id: 'r-文字2',
+  type: 'text',
+  src: 'e-文字2',
+  operation: [
+    {
+      type: 'insert-brother',
+      nextId: 'r-文字3',
+      position: 'back-flow',
+      offsetIndex: 1,
+    },
+  ],
+}
+
+const e文字2: IElement = {
+  id: 'e-文字2',
+  type: 'text',
+  content: '我做的',
+  operation: [
+    {
+      type: 'start-mark-pointer',
+      markId: 'mark-0',
+      offsetIndex: 0,
+    },
+  ],
+}
+
+const mark0: IMarkData = {
+  id: 'mark-0',
+  data: {
+    backgroundColor: 'yellow',
+  },
+}
+
+const r文字3: IPieceReplacedElement = {
+  id: 'r-文字3',
+  type: 'text',
+  src: 'e-文字3',
+}
+
+const e文字3: IElement = {
+  id: 'e-文字3',
+  type: 'text',
+  content: '喻碧云',
+  operation: [],
+}
+
+const rFLowChart: IReplacedElement = {
+  id: 'r-flow-chart-0',
+  type: 'flow-chart',
+  src: 'e-flow-chart-0',
+}
+
+const eFlowChart: IElement = {
+  id: 'e-flow-chart-0',
+  type: 'flow-chart',
+  operation: [],
+}
+
+const rTable: IReplacedElement = {
+  id: 'r-table0',
+  type: 'table',
+  src: 'e-table0',
+}
+const eTable: IElement = {
+  id: 'e-table0',
+  type: 'table',
+  operation: [
+    {
+      type: 'insert-child',
+      position: 'back-flow',
+      nextId: 'r-row0',
+    },
+    {
+      type: 'insert-child',
+      position: 'back-flow',
+      nextId: 'r-col0',
+    },
+    {
+      type: 'teleport-insert-child',
+      nextId: 'r-文字4',
+      position: 'back-flow',
+      coordinate: ['e-row0', 'e-col0'],
+    },
+    {
+      type: 'teleport-insert-child',
+      nextId: 'r-文字5',
+      position: 'back-flow',
+      coordinate: ['e-row0', 'e-col3'],
+    },
+    {
+      type: 'start-teleport-mark-pointer',
+      markId: 'mark2',
+      coordinate: ['e-row0', 'e-col0'],
+    },
+    {
+      type: 'end-teleport-mark-pointer',
+      markId: 'mark2',
+      coordinate: ['e-row0', 'e-col3'],
+    },
+  ],
+}
+
+const mark2: IMarkData = {
+  id: 'mark2',
+  data: {
+    backgroundColor: 'red',
+  },
+}
+
+const rRow0: IReplacedElement = {
+  id: 'r-row0',
+  type: 'row',
+  src: 'e-row0',
+  operation: [
+    {
+      type: 'insert-brother',
+      position: 'back-flow',
+      nextId: 'r-row1',
+    },
+    {
+      type: 'insert-brother',
+      position: 'front-flow',
+      nextId: 'r-row2',
+    },
+  ],
+}
+
+const eRow0: IElement = {
+  id: 'e-row0',
+  type: 'row',
+}
+
+const rRow1: IReplacedElement = {
+  id: 'r-row1',
+  type: 'row',
+  src: 'e-row1',
+}
+
+const eRow1: IElement = {
+  id: 'e-row1',
+  type: 'row',
+}
+
+const rRow2: IReplacedElement = {
+  id: 'r-row2',
+  type: 'row',
+  src: 'e-row2',
+}
+
+const eRow2: IElement = {
+  id: 'e-row2',
+  type: 'row',
+  operation: [],
+}
+
+const rCol0: IReplacedElement = {
+  id: 'r-col0',
+  type: 'col',
+  src: 'e-col0',
+  operation: [
+    {
+      type: 'insert-brother',
+      position: 'back-flow',
+      nextId: 'r-col1',
+    },
+    {
+      type: 'insert-brother',
+      position: 'back-flow',
+      nextId: 'r-col2',
+    },
+    {
+      type: 'insert-brother',
+      position: 'back-flow',
+      nextId: 'r-col3',
+    },
+  ],
+}
+
+const eCol0: IElement = {
+  id: 'e-col0',
+  type: 'col',
+  operation: [
+    {
+      type: 'element-mark',
+      markId: 'mark-1',
+    },
+  ],
+}
+
+const mark1: IMarkData = {
+  id: 'mark-1',
+  data: {
+    width: 300,
+  },
+}
+
+const rCol1: IReplacedElement = {
+  id: 'r-col1',
+  type: 'col',
+  src: 'e-col1',
+}
+
+const eCol1: IElement = {
+  id: 'e-col1',
+  type: 'col',
+  operation: [],
+}
+
+const rCol2: IReplacedElement = {
+  id: 'r-col2',
+  type: 'col',
+  src: 'e-col2',
+}
+
+const eCol2: IElement = {
+  id: 'e-col2',
+  type: 'col',
+  operation: [],
+}
+
+const rCol3: IReplacedElement = {
+  id: 'r-col3',
+  type: 'col',
+  src: 'e-col3',
+}
+
+const eCol3: IElement = {
+  id: 'e-col3',
+  type: 'col',
+  operation: [],
+}
+
+const r文字4: IPieceReplacedElement = {
+  id: 'r-文字4',
+  type: 'text',
+  src: 'e-文字4',
+}
+
+const e文字4: IElement = {
+  id: 'e-文字4',
+  type: 'text',
+  content: '第一个格子',
+  operation: [],
+}
+
+const r文字5: IPieceReplacedElement = {
+  id: 'r-文字5',
+  type: 'text',
+  src: 'e-文字5',
+}
+
+const e文字5: IElement = {
+  id: 'e-文字5',
+  type: 'text',
+  content: '第二个格子',
+  operation: [],
+}
