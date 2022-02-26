7d678f7d4032b06bd6ea0ef83e9b66c6c002438a
湫曗 <autumnsilverleaves@gmail.com>
2022-02-26 19:06:33 +0800

    feat: basic compose commands usecase

--- packages/moonman/src/doc/createDocument.ts
+++ packages/moonman/src/doc/createDocument.ts
@@ -1,13 +1,42 @@
 import { IPlanet } from '@moonman/blueprint/src/blueprint'
 import { createPlanet } from '@moonman/nebula'
-import { addMarkToPlanet } from '../../../command/src/addMarkToPlanet'
+import { addMarkToPlanet } from '@moonman/command'
 import { runCommandsWithTransation } from '@moonman/command'
 export function createDocumentByPlanet(planet: IPlanet) {
   return addMarkToPlanet(planet, 'type', 'document')
 }
 
-export const createDocument = () => {
-  const document = createPlanet()
-  runCommandsWithTransation([addMarkToPlanet(document, 'type', 'document')])
-  return document
+export function createDocument() {
+  const nodePlanet = createPlanet()
+  runCommandsWithTransation([addMarkToPlanet(nodePlanet, 'type', 'document')])
+  return nodePlanet
+}
+
+export function createParagraph() {
+  const nodePlanet = createPlanet()
+  runCommandsWithTransation([addMarkToPlanet(nodePlanet, 'type', 'pargraph')])
+  return nodePlanet
+}
+
+export function createImage(src: string) {
+  const nodePlanet = createPlanet()
+  runCommandsWithTransation([
+    addMarkToPlanet(nodePlanet, 'type', 'image'),
+    addMarkToPlanet(nodePlanet, 'src', 'image'),
+  ])
+  return nodePlanet
+}
+
+export function createText(text: string) {
+  const commands = text
+    .split('')
+    .map((char, index) => {
+      const nodePlanet = createPlanet()
+      return [
+        addMarkToPlanet(nodePlanet, 'type', 'text'),
+        addMarkToPlanet(nodePlanet, 'content', char),
+      ]
+    })
+    .flat()
+  runCommandsWithTransation(commands)
 }
