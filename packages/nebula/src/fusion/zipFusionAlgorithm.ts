eba006441dc2619e008efaefcf5789d834d347a1
湫曗 <autumnsilverleaves@gmail.com>
2022-03-12 01:40:10 +0800

    feat: add zipFusionR

--- packages/nebula/src/fusion/zipFusionAlgorithm.ts
+++ packages/nebula/src/fusion/zipFusionAlgorithm.ts
@@ -32,3 +32,41 @@ export function zipFusion<T>(
 
   return newArray
 }
+
+// 因为新的操作更多场景出现，所以反向操作在 side2 数量少的时候会需要更少次数的对比
+export function zipFusionR<T>(
+  side1: T[],
+  side2: T[],
+  compare: (value1: T, value2: T) => number,
+): T[] {
+  let side1CurrentIndex = side1.length - 1
+  let side2CurrentIndex = side2.length - 1
+
+  const newArray = []
+
+  while (side1CurrentIndex > -1 && side2CurrentIndex > -1) {
+    const side1Value = side1[side1CurrentIndex]
+    const side2Value = side2[side2CurrentIndex]
+
+    if (compare(side1Value, side2Value) > 0) {
+      newArray.push(side2Value)
+      side2CurrentIndex--
+    } else if (compare(side1Value, side2Value) < 0) {
+      newArray.push(side1Value)
+      side1CurrentIndex--
+    } else {
+      newArray.push(side1Value)
+      side1CurrentIndex--
+      side2CurrentIndex--
+    }
+  }
+
+  newArray.push(
+    ...side1.slice(side1CurrentIndex).reverse(),
+    ...side2.slice(side2CurrentIndex).reverse(),
+  )
+
+  newArray.reverse()
+
+  return newArray
+}
